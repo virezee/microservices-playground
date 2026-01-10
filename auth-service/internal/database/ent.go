e3ebe69cba3b343937830df8e26cd83287254ea0
Zee <56460839+virezee@users.noreply.github.com>
2026-01-10 21:55:48 +0700

    learn db temp

--- auth-service/internal/database/ent.go
+++ auth-service/internal/database/ent.go
@@ -1,5 +1,16 @@
 package database
 
-func EntClient()  {
+import (
+	"database/sql"
+	"os"
 
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+)
+
+func EntClient() *ent.Client {
+	dsn := os.Getenv("DB_URL")
+	db, _ := sql.Open("pgx", dsn)
+	driver := entsql.OpenDB(dialect.Postgres, db)
+	return ent.NewClient(ent.Driver(driver))
 }
